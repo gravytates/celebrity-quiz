7c097a6acb3c0b5fb275560c5a905ba52ae3d6f7
Grady Shelton and Sean Pierce <grady@>
2017-03-23 11:15:09 -0700

    Create JavaScript logic to handle user input, and display appropriate information

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,38 @@
 $(document).ready(function(){
 
-  var name = $("#name input").val();
-  var age = parseInt($("#age input").val());
-  var entertainment = $("#entertainment").val();
+  $("#questionaire").submit(function(event) {
 
+    var name = $("#name input").val();
+    var age = parseInt($("#age input").val());
+    var entertainment = $("#entertainment").val();
+
+    event.preventDefault();
+
+    $("#form-wrapper").hide();
+    // check if user young or seasoned
+    if (age >= 40) {
+
+      if (entertainment === "music") {
+        $("#old-music").show();
+      } else if (entertainment === "books") {
+        $("#old-books").show();
+      } else {
+        $("#old-movies").show();
+      }
+
+    } else {
+
+      if (entertainment === "music") {
+        $("#young-music").show();
+      } else if (entertainment === "books") {
+        $("#young-books").show();
+      } else {
+        $("#young-movies").show();
+      }
+
+
+
+    }; // end conditionals
+
+  });
 });
